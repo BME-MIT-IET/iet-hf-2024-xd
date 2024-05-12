1d84421697230928cf493575cd85f8e31b0f30e0
Hölbling Botond <botetta@gmail.com>
2024-05-12 18:40:01 +0200

    jacoco setup + cso tesztek kiegeszitese

--- drukmakor/src/test/java/org/drukmakor/Tests.java
+++ drukmakor/src/test/java/org/drukmakor/Tests.java
@@ -134,6 +134,15 @@ class CsoTests{
         assertEquals(1, cso.getVizmennyiseg()); // A csoben maximum 1 egység viz lehet, tehát továbbra is 1 egység van benne
     }
 
+    @DisplayName("VizetNovel nem mukodo cso")
+    @Test
+    void testVizetNovel2() throws Exception {
+        Cso cso = new Cso();
+        cso.Kilyukaszt(); // Kilyukasztjuk a csot
+        cso.VizetNovel(1); // 1 egység vizet pumpalunk a csobe
+        assertEquals(0, cso.getVizmennyiseg()); // A cso nem mukodik, ezert nem lehet vizet pumpalni bele
+    }
+
     @DisplayName("VizetCsokkent")
     @Test
     void testVizetCsokkent() throws Exception {
@@ -144,4 +153,82 @@ class CsoTests{
         cso.VizetCsokkent(1); // még 1 egység vizet megprobálunk kivenni a csobol
         assertEquals(0, cso.getVizmennyiseg()); // A cso tartalma tovabbra is 0, mert nem lehet negativ vizmennyiseg
     }
+
+    @DisplayName("Szomszed hozzaadas")
+    @Test
+    void testSzomszedHozzaad() {
+        Cso cso = new Cso();
+        Pumpa pumpa = new Pumpa();
+        cso.SzomszedHozzaad(pumpa); // Hozzaadjuk a pumpa-t a cso szomszedaihoz
+        assertEquals(1, cso.GetSzomszedok().size()); // A cso egy szomszeddal rendelkezik
+        assertEquals(pumpa, cso.GetSzomszedok().get(0)); // A szomszed a cso2
+        //Második pumpa szomszéd hozzáadása
+        Pumpa pumpa2 = new Pumpa();
+        cso.SzomszedHozzaad(pumpa2); // Hozzaadjuk a pumpa2-t a cso szomszedaihoz
+        assertEquals(2, cso.GetSzomszedok().size()); // A cso ket szomszeddal rendelkezik
+        assertEquals(pumpa2, cso.GetSzomszedok().get(1)); // A masodik szomszed a pumpa2
+        //Ha a cso mar ket szomszeddal rendelkezik, akkor nem tudunk tobbet hozzaadni
+        Ciszterna c = new Ciszterna();
+        cso.SzomszedHozzaad(c); // Hozzaadjuk a ciszternat a cso szomszedaihoz
+        assertEquals(2, cso.GetSzomszedok().size()); // A cso tovabbra is ket szomszeddal rendelkezik
+        assertFalse(cso.GetSzomszedok().contains(c)); // A cso szomszedai kozott nincs a ciszterna
+
+    }
+
+    @DisplayName("PumpaEpit")
+    @Test
+    void testPumpaEpit() {
+        Cso cso = new Cso();
+        Ciszterna c = new Ciszterna();
+        cso.SzomszedHozzaad(c); //Ez a ciszterna azért kell mert a pumpaepit függvényben a cso szomszédait vizsgáljuk (Kell hogy legyen legalább egy szomszéd)
+        Szerelo szerelo = new Szerelo();
+        szerelo.getPumpaHatizsak().add(new Pumpa()); // A szerelo hátizsákjában van egy pumpa
+        szerelo.Lep(cso); // A szerelo ralep a csore
+        cso.PumpaEpit(); // A csohoz hozzaepitjuk a pumpat
+        assertEquals(1, cso.GetSzomszedok().size()); // A cso egy szomszeddal rendelkezik
+        assertInstanceOf(Pumpa.class, cso.GetSzomszedok().get(0)); // A szomszed egy pumpa
+        assertEquals(0, szerelo.getPumpaHatizsak().size()); // A szerelo hátizsákjában nincs tobb pumpa
+        assertEquals(2, cso.GetSzomszedok().get(0).GetSzomszedok().size()); // A pumpa ket szomszeddal rendelkezik
+    }
+
+    @DisplayName("Csuszos csorol tovabb csuszik a jatekos")
+    @Test
+    void testCsuszosCsorolCsuszik() {
+        // A veletlen esemenyeket kikapcsoljuk ehhez a teszthez, mert a csuszas veletlenszeru szomszedra csusztatja a jatekost
+        Mezo.doRandomThings = false; //Ha ezt kikapcsoltuk akkor a jatekos mindig az elso szomszedra csuszik (egyebkent veletlenszeruen a két szomszéd közül)
+        Cso cso = new Cso();
+        Pumpa pumpa = new Pumpa();
+        Pumpa pumpa2 = new Pumpa();
+        cso.Csuszik(); // A csot csuszossa tesszuk, ez azt jelenti hogy ha rálép valaki akokr tovább fog csúszni az első szomszédra
+        cso.SzomszedHozzaad(pumpa); // Ez az elso szomszed, erre fog csuszni a jatekos
+        cso.SzomszedHozzaad(pumpa2);
+        Szerelo szerelo = new Szerelo();
+        szerelo.Lep(cso); // A szerelo ralep a csore, de mivel csuszossa tettuk a csot, ezert tovabb fog csuszni
+        assertEquals(pumpa, szerelo.getHelyzet()); // A szerelo a pumpa-ra csuszott
+    }
+
+    @DisplayName("Csuszossag elmúltával a jatekos nem csuszik tovább")
+    @Test
+    void testCsuszosCsorolCsuszik2() throws Exception {
+        // A veletlen esemenyeket kikapcsoljuk ehhez a teszthez, mert a csuszas veletlenszeru szomszedra csusztatja a jatekost
+        Mezo.doRandomThings = false; //Ha ezt kikapcsoltuk akkor a jatekos mindig az elso szomszedra csuszik (egyebkent veletlenszeruen a két szomszéd közül)
+        Cso cso = new Cso();
+        Pumpa pumpa = new Pumpa();
+        Pumpa pumpa2 = new Pumpa();
+        cso.Csuszik(); // A csot csuszossa tesszuk, ez azt jelenti hogy ha rálép valaki akokr tovább fog csúszni az első szomszédra
+        cso.SzomszedHozzaad(pumpa); // Ez az elso szomszed, erre fog csuszni a jatekos
+        cso.SzomszedHozzaad(pumpa2);
+        Szerelo szerelo = new Szerelo();
+        szerelo.Lep(cso); // A szerelo ralep a csore, de mivel csuszossa tettuk a csot, ezert tovabb fog csuszni
+        assertEquals(pumpa, szerelo.getHelyzet()); // A szerelo a pumpa-ra csuszott
+        // 5 frissítés után a csuszosság el fog múlni
+        for (int i = 0; i < 5; i++) {
+            cso.Frissit();
+        }
+        assertEquals(0, cso.getCsuszos()); // A csuszossag elmult
+        szerelo.Lep(cso); // A szerelo ralep a csore, de mar nem fog tovabb csuszni
+        assertEquals(cso, szerelo.getHelyzet()); // A szerelo a cso-ra lepett
+    }
+
+
 }
