5865408132dfcee067bba31dd110cc400e645344
lyozi <124312789+lyozi@users.noreply.github.com>
2024-05-14 14:57:00 +0200

    Added basic tests for Dark mode and input test

---
+++ src/test/java/UITests/DarkModeTest.java
@@ -0,0 +1,76 @@
+package UITests;
+
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.drukmakor.Grafika;
+import org.drukmakor.ParancsErtelmezo;
+import org.drukmakor.ParancsErtelmezoView;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.assertj.swing.core.Settings;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DarkModeTest extends AssertJSwingJUnitTestCase {
+    private FrameFixture window;
+
+    @Override
+    protected void onSetUp() {
+        Settings settings = new Settings();
+        settings.delayBetweenEvents(10);
+        // Dummy példányok létrehozása
+        ParancsErtelmezoView dummyPEV = new ParancsErtelmezoView();
+        ParancsErtelmezo dummyPE = new ParancsErtelmezo(dummyPEV);
+
+        dummyPE.runFromFile("commandfiles/alap");
+        dummyPE.EnableDebugMode(false);
+        dummyPE.OutputToView(true);
+
+        Grafika frame = GuiActionRunner.execute(() -> new Grafika(dummyPEV, dummyPE));
+
+        window = new FrameFixture(robot(), frame);
+        window.show();
+    }
+
+    @Test
+    public void shouldTurnOnDarkMode() {
+        // A sötét mód bekapcsolása
+        window.panel("pGridBag").textBox("tfInput").enterText("dark");
+        window.button("bSend").click();
+
+        // Fő panel
+
+        assertTrue(window.textBox("jtaOutput").background().target().equals(new Color(80, 80, 80)));
+
+        assertTrue(window.panel("pGridBag").background().target().equals(new Color(150, 150, 150)));
+
+        assertTrue(window.button("bSend").background().target().equals(new Color(100, 100, 100)));
+
+        // Új játék gomb
+        assertTrue(window.button("bNewGame").background().target().equals(new Color(100, 100, 100)));
+
+        // Új egyedi pálya betöltése gomb
+        assertTrue(window.button("bNewUniqueGame").background().target().equals(new Color(100, 100, 100)));
+
+        // Súgó gomb
+        assertTrue(window.button("bHelp").background().target().equals(new Color(100, 100, 100)));
+
+        assertTrue(window.panel("pCantSee").background().target().equals(new Color(100, 100, 100)));
+
+        // Segédpanel 1
+        assertTrue(window.panel("pCantSee2").background().target().equals(new Color(100, 100, 100)));
+
+        // Bemeneti mező
+        assertTrue(window.textBox("tfInput").background().target().equals(new Color(100, 100, 100)));
+    }
+
+}
